8f4c2befe7724df6f05881ddc2fc53e439535540
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-23 21:23:45 +0200

    tasks

--- markdown2html.py
+++ markdown2html.py
@@ -4,13 +4,26 @@
 
 from sys import argv, stderr
 from os.path import exists
+from markdown import markdown
 
-if (len(argv) < 3):
-    print("Usage: ./markdown2html.py README.md README.html", file = stderr)
-    exit(1)
+errMessage = "Usage: ./markdown2html.py README.md README.html"
 
-elif (not exists(f"{argv[1]}")):
-    print(f"Missing {argv[1]}", file = stderr)
+if __name__ == "__main__":
+    if (len(argv) < 3):
+        print(f"{errMessage}", file=stderr)
+        exit(1)
 
-else:
-    exit(0)
+    elif (not exists(f"{argv[1]}")):
+        print(f"Missing {argv[1]}", file=stderr)
+
+    else:
+        # Open the markdown file & take it's content
+        file = open(f"{argv[1]}", "r")
+        content = file.read()
+        html_rep = markdown(content)
+        file.close()
+
+        with open(f"{argv[2]}", "w") as file:
+            file.write(html_rep)
+        file.close()
+        exit(0)
