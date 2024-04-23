cfb2e7346c077e3cadc6d8fdd3bae2dca9b5fff2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-23 21:41:12 +0200

    tasks

--- markdown2html.py
+++ markdown2html.py
@@ -2,7 +2,7 @@
 """ 0. Start a script """
 
 
-from sys import argv, stderr
+from sys import argv, stderr, exit
 from os.path import exists
 from markdown import markdown
 
@@ -15,15 +15,13 @@ if __name__ == "__main__":
 
     elif (not exists(f"{argv[1]}")):
         print(f"Missing {argv[1]}", file=stderr)
+        exit(1)
 
     else:
-        # Open the markdown file & take it's content
-        file = open(f"{argv[1]}", "r")
-        content = file.read()
-        html_rep = markdown(content)
-        file.close()
+        with open(f"{argv[1]}", "r") as file:
+            content = file.read()
+            html_rep = markdown(content)
 
         with open(f"{argv[2]}", "w") as file:
             file.write(html_rep)
-        file.close()
         exit(0)
