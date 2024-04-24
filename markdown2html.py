fdf086b145247be2bfd9eee6b9ef3d68be102d9b
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-24 18:13:58 +0200

    tasks

--- markdown2html.py
+++ markdown2html.py
@@ -1,27 +1,62 @@
 #!/usr/bin/python3
-""" 0. Start a script """
+"""
+This is a script to convert a Markdown file to HTML.
+
+Usage:
+    ./markdown2html.py [input_file] [output_file]
+
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
+"""
+
+import argparse
+import pathlib
+import re
 
 
-from sys import argv, stderr, exit
-from os.path import exists
-from markdown import markdown
+def convert_md_to_html(input_file, output_file):
+    '''
+    Converts markdown file to HTML file
+    '''
+    # Read the contents of the input file
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
 
-errMessage = "Usage: ./markdown2html.py README.md README.html"
+    html_content = []
+    for line in md_content:
+        # Check if the line is a heading
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            # Get the level of the heading
+            h_level = len(match.group(1))
+            # Get the content of the heading
+            h_content = match.group(2)
+            # Append the HTML equivalent of the heading
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
 
-if __name__ == "__main__":
-    if (len(argv) < 3):
-        print(f"{errMessage}", file=stderr)
-        exit(1)
+    # Write the HTML content to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
 
-    elif (not exists(f"{argv[1]}")):
-        print(f"Missing {argv[1]}", file=stderr)
-        exit(1)
 
-    else:
-        with open(f"{argv[1]}", "r") as file:
-            content = file.read()
-            html_rep = markdown(content)
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
 
-        with open(f"{argv[2]}", "w") as file:
-            file.write(html_rep)
-        exit(0)
+    # Check if the input file exists
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
+        sys.exit(1)
+
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
